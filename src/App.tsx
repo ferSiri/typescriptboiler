b7a13f5460ee1e7a716244801cf548b83281e0ad
@omarsiri88 <omarsiri88@gmail.com>
2022-03-14 16:23:03 -0300

    implementado axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 
 const App : React.FC = () => {
+
+    async function getUser() {
+        try {
+          const response = await axios.get('https://jsonplaceholder.typicode.com/users', {headers: {'Content-Type': 'application/json'}});
+          console.log(response);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+    useEffect(()=>{
+        getUser();
+    },[])
 
     return (
             <div>
